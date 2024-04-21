010b17d0d68d64a6f5d809034d6fce22be8a782c
endomorphosis <starworks5@gmail.com>
2024-04-20 18:05:47 -0700

    update

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup
 
 setup(
-	name='ipfs-datasets',
 	version='0.0.1',
 	packages=[
 		'ipfs_datasets',
-		'ipfs_datasets.ipfs_kit_lib',
-
+		'ipfs_datasets.model_manager',
+        'ipfs_datasets.model_manager.ipfs_kit_lib',
+		'ipfs_datasets.model_manager.orbitdb_kit_lib',
 	],
 	install_requires=[
 		'datasets',
+        "ipfs_model_manager@git+https://github.com/endomorphosis/ipfs_model_manager.git",
+        "orbitdb_kit@git@git+https://github.com/endomorphosis/orbitdb_kit.git",
+		"ipfs_kit@git+https://github.com/endomorphosis/ipfs_kit.git",
+		"faiss",
 		'urllib3',
 		'requests',
 		'boto3',
