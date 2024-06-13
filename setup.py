9fdd7da6316aebcad8571752c9c43741ee5c6c3b
endomorphosis <starworks5@gmail.com>
2024-06-12 23:33:26 -0700

    Refactor IPFS-related code and update dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup
 
 setup(
+    name="ipfs_datasets",
 	version='0.0.1',
 	packages=[
-		'ipfs_datasets',
+		'ipfs_datasets'
 	],
 	install_requires=[
-		'datasets',
-        "ipfs_model_manager@git+https://github.com/endomorphosis/ipfs_model_manager.git",
-        "orbitdb_kit@git@git+https://github.com/endomorphosis/orbitdb_kit.git",
-		"ipfs_kit@git+https://github.com/endomorphosis/ipfs_kit.git",
-		"faiss",
-		'urllib3',
-		'requests',
-		'boto3',
+        'orbitdb_kit@git+https://github.com/endomorphosis/orbitdb_kit.git',
+        'ipfs_kit@git+https://github.com/endomorphosis/ipfs_kit.git',
+        'ipfs_model_manager@git+https://github.com/endomorphosis/ipfs_model_manager.git',
+        'transformers',
+        'numpy',
+        'urllib3',
+        'requests',
+        'boto3',
 	]
 )
