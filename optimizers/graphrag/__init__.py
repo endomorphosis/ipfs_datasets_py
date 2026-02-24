0eccc1b37bb7653d9c8547daff0553e354ea94d8
Benjamin Barber <starworks5@gmail.com>
2026-02-24 01:03:37 -0800

    feat: add OntologyComparator for ontology comparison and ranking functionality

--- optimizers/graphrag/__init__.py
+++ optimizers/graphrag/__init__.py
@@ -1,9 +1,11 @@
 """GraphRAG optimizer modules."""
 
 from .score_analyzer import ScoreAnalyzer, DimensionStats, STANDARD_DIMENSIONS
+from .ontology_comparator import OntologyComparator
 
 __all__ = [
     'ScoreAnalyzer',
     'DimensionStats',
     'STANDARD_DIMENSIONS',
+    'OntologyComparator',
 ]
