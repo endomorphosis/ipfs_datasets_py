dd898fe669ca9d4740fafe5123bf5f6ac91c7bb0
benjamin barber <starworks5@gmail.com>
2026-08-23 18:30:27 +0000

    chore(eaaef): remove unowned top-level package aliases

--- analysis/external_agent_incremental_refresh.py
+++
@@ -1,9 +0,0 @@
-"""Re-export EAAEF-101 refresh."""
-
-from ipfs_datasets_py.analysis.external_agent_incremental_refresh import (  # type: ignore[import-not-found]
-    INDEXES,
-    RefreshError,
-    refresh,
-)
-
-__all__ = ("INDEXES", "RefreshError", "refresh")
--- retrieval/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Merge-admission package alias for EAAEF-060 federated retrieval contracts."""
-
-from .agent_work_contracts import *  # noqa: F403
-from .agent_work_contracts import __all__ as __all__
